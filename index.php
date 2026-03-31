8f5fbd65eb47f4de199d80e1b6832f7ddde64ab0
YusufAksari <u8033194811@gmail.com>
2026-03-31 10:57:13 +0200

    index van de website

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+
+<link rel="stylesheet" href="css/style.css">
+
+<section class="home">
+    <h2>Welkom bij Yusuf's website</h2>
+    <p>Hier kan je de beste Turkse dranken bestellen</p>
+</section>
+
+<footer>
+    <p>All rights reserved - Yusuf Bugra Aksari</p>
+</footer>
