15ccd54f17f5ff9548d65947ef6ec56bb8c58d01
YusufAksari <u8033194811@gmail.com>
2026-03-31 10:56:43 +0200

    header aangemaakt

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="logo">
+        <img src="/turkiye-drinks/assets/images/flag.png">
+        <h1>The Turkiye Drinks</h1>
+    </div>
+
+    <nav>
+        <a href="/turkiye-drinks/index.php">Home</a>
+        <a href="/turkiye-drinks/pages/products.php">Producten</a>
+        <a href="/turkiye-drinks/pages/cart.php">Winkelmand</a>
+    </nav>
+</header>
